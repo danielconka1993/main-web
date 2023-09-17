cfc82d537d4d8c0d2b72d93b0ec58216e549a6c2
Daniel Čonka <pykejavzlykej@gmail.com>
2023-09-17 20:02:00 +0200

    Projekty - Vytvořen odkaz na starý web

--- src/pages/Projekty.js
+++ src/pages/Projekty.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import "./css/Projekty.css"
+import data from "./Projekty/data"
 
 const Projekty = () => {
-  return (
-    <div>Projekty</div>
-  )
+  return <section className='projekty'>
+    {
+      data.map( (oneProjekt) => {
+        const {id,img, url, title, info} = oneProjekt
+
+        return <article className='jeden-projekt' key={id}>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+          <img src={img} alt="obrazek projektu" />
+          {/* <iframe src={url} width="80%" height="70%" title="External Page"></iframe> */}
+          <h1>{title}</h1></a>
+          <strong>{info}</strong>
+        </article>
+      })
+    }
+    
+  </section>
 }
 
 export default Projekty
