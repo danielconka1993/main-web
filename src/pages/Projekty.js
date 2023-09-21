114c9432f41160ebbe76a1281e6a3d9bebd61e5a
Daniel Čonka <pykejavzlykej@gmail.com>
2023-09-21 18:53:22 +0200

    Projekty - Přidán projekt Form1

--- src/pages/Projekty.js
+++ src/pages/Projekty.js
@@ -3,19 +3,21 @@ import "./css/Projekty.css"
 import staryWeb from "./Projekty/staryweb.png"
 import Netflix from "./Projekty/Netflix/Netflix"
 import ISS from "./Projekty/API/ISS"
+import NoFirebase1 from "./Projekty/Form/noFirebase1/Form1"
 
 
 
 const Projekty = () => {
   const [zobrazitProjektNetflix,setZobrazitProjektNetflix] = useState(false)
   const [zobrazitProjektAPI, setZobrazitProjektAPI] = useState(false)
+  const [zobrazitProjektForm_noFireBase, setZobrazitProjektForm_noFireBase] = useState(true)
   return <section className='projekty'>
         <section className="vsechny-projekty">
 
           <article className="container-jazyk"> 
             <h1>React</h1>
-{/* !!!!!!!!!!!!!!!!! -     Nezapomenout upravit při novém projektu  - !!!!!!!!!!!!!!!!!!*/}
-            <div className={`projekty-react ${(zobrazitProjektNetflix || zobrazitProjektAPI) ? "projektyReact-sloupec" : "projektyReact-radek"}`}>
+{/* Nezapomenout upravit při novém projektu - Dobudoucna předělat na useReducer*/}
+            <div className={`projekty-react ${(zobrazitProjektNetflix || zobrazitProjektAPI || zobrazitProjektForm_noFireBase) ? "projektyReact-sloupec" : "projektyReact-radek"}`}>
 
               {/* Netflix */}
               <div className={`jeden-projekt ${zobrazitProjektNetflix ? "jedenprojekt-big" : "jedenprojekt-small"}`}>
@@ -36,16 +38,25 @@ const Projekty = () => {
                 </button>
                 {zobrazitProjektAPI && <ISS />}
               </div>
+
+              {/* Form_noFireBase */}
+              <div className={`jeden-projekt ${zobrazitProjektForm_noFireBase ? "jedenprojekt-big" : "jedenprojekt-small"}`}>
+                <h3>Formulář 1</h3>
+                <button className="btn-jedenProjekt" onClick={() => {setZobrazitProjektForm_noFireBase(!zobrazitProjektForm_noFireBase);}}>
+                    {zobrazitProjektForm_noFireBase ? "Skrýt projekt" : "Zobrazit projekt"}
+                </button>
+                {zobrazitProjektForm_noFireBase && <NoFirebase1 />}
+              </div>
             </div>
           </article>
 
           <article className="container-jazyk">
             <h1>JS | PHP | SQL</h1>
-            <div className='jeden-projekt'>
+            <div className='jeden-projekt staryweb'>
               <a href="https://arevyhs.000webhostapp.com" target="_blank" rel="noopener noreferrer">
               <img src={staryWeb} alt="obrazek projektu" />
               <h3>Starý web</h3></a>
-              <strong>Původní web s projekty a odesílacím formulářem na můj email</strong>
+              <strong>Původní web s projekty a odesílacím formulářem na můj email.</strong>
             </div>
           </article>
 
