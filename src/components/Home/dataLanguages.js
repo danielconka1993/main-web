9ceba39f7f3c86ba5fb0ce3f8d432abf73b1b6ac
Daniel Čonka <pykejavzlykej@gmail.com>
2024-01-14 18:24:30 +0100

    languages update

--- src/components/Home/dataLanguages.js
+++ src/components/Home/dataLanguages.js
@@ -4,43 +4,38 @@ import "./Right/css/AllLanguages.css";
 const dataLanguages = [
   {
     id: 1,
-    buttonValue: "JavaScript",
+    buttonValue: "ReactJS",
     jsxValue: (
       <article className="oneLanguageDetail">
-        <h1>JavaScript</h1>
+        <h1>ReactJS</h1>
         <h2>Při práci s ním ovládám </h2>
-        <p>Short circuit evaluation, cykly, funkce.</p>
         <p>
-          Práci s <span className="span-yellowText">polem</span> -
-          destructuringu, .map, .filter, .find, spread operátor.
+          <span className="span-yellowText">Globál</span> Context + Reducer, props. Ukládáná dat z BE do  <span className="span-underLine">LocalStorage</span>.
         </p>
         <p>
-          <span className="span-yellowText">OOP</span>, práci s API, manipulace
-          s <span className="span-yellowText">DOM</span> - HTML, CSS, eventy.
+          <span className="span-yellowText">Hooks</span> všeho druhu. Tvorba <span className="span-yellowText">Redexů</span>. Zpracování <span className="span-yellowText">REST API</span>.
+        </p>
+        <p>
+          <span className="span-yellowText">FireBase</span> - správa databáze, formuláře, dynamické MPA.
         </p>
       </article>
     ),
   },
   {
     id: 2,
-    buttonValue: "PHP & SQL",
+    buttonValue: "Node.js",
     jsxValue: (
       <article className="oneLanguageDetail">
-        <h1>
-          PHP <span className="span-yellowText">&</span> SQL
-        </h1>
+        <h1>Node.js - Express</h1>
         <h2>Při práci s ním ovládám </h2>
         <p>
-          <span className="span-yellowText">Správu dat a tabulek </span>
-          (referenční, omezujicí integrita) v databáze, Cookies, Sessions.
+          <span className="span-yellowText">Routy</span> - GET, PUT, DELETE, POST, Create. Upřednosťuji <span className="span-yellowText">try-catch konstrukci</span>.
         </p>
         <p>
-          <span className="span-yellowText"> Bezpečnos</span> - escapování,
-          trimování, hashování hesel a prevence XSS.
+          <span className="span-underLine">Bezpečnost</span> - <span className="span-yellowText">JWT</span> token, <span className="span-yellowText">HASH</span> hesel.
         </p>
         <p>
-          <span className="span-yellowText">OOP</span> a využití vestavěných
-          funkcí.
+          Efektivní manipulace v routě s větším počtem kolekcí v <span className="span-yellowText">MongoDB</span> pro minimalizaci počtu endpointů .
         </p>
       </article>
     ),
@@ -54,6 +49,9 @@ const dataLanguages = [
           Git <span className="span-yellowText">&</span> GitHub
         </h1>
         <h2>Při práci s ním ovládám </h2>
+        <p>
+        <span className="span-yellowText">Git</span> - práce s terminálem.
+        </p>
         <p>
           <span className="span-yellowText">GUI</span> - používání grafického
           prostředí <span className="span-underLine">Git Kraken</span>.
@@ -73,49 +71,53 @@ const dataLanguages = [
   },
   {
     id: 4,
-    buttonValue: "ReactJS",
+    buttonValue: "JavaScript",
     jsxValue: (
       <article className="oneLanguageDetail">
-        <h1>React</h1>
+        <h1>JavaScript</h1>
         <h2>Při práci s ním ovládám </h2>
+        <p>Short circuit evaluation, cykly, funkce, metody,..</p>
         <p>
-          <span className="span-yellowText">FireBase</span> - správa databáze
-          (také posluchač), formuláře, dynamické MPA.
+          Práci s <span className="span-yellowText">polem & objekty</span> -
+          destructuring, .map, .filter, .find, spread operátor, ...
         </p>
         <p>
-          <span className="span-yellowText">Hooks</span> - useState, useEffect,
-          useRaf, useReducer, useContext, useParams, tvoba{" "}
-          <span className="span-yellowText">vlasních Hooků</span> +
-          destructuring.
-        </p>
-        <p>
-          <span className="span-yellowText">eventy</span> - onClick, onChange,
-          také Prop Drilling a icony.
+          <span className="span-yellowText">OOP</span>, práci s API, manipulace
+          s <span className="span-yellowText">DOM</span> - HTML, CSS, eventy.
         </p>
       </article>
     ),
   },
   {
     id: 5,
-    buttonValue: "TypeScript",
+    buttonValue: "PHP & SQL",
     jsxValue: (
       <article className="oneLanguageDetail">
-        <h1>TypeScript</h1>
+        <h1>
+          PHP <span className="span-yellowText">&</span> SQL
+        </h1>
         <h2>Při práci s ním ovládám </h2>
         <p>
-          <span className="span-yellowText">Datové typy</span> - Literal type,
-          custom type, enum, void, never, unknown, Function, tuple, any, object
-          + základní typy.
+          <span className="span-yellowText">Správu dat a tabulek </span>
+          (referenční, omezujicí integrita) v databáze, Sessions.
+        </p>
+        <p>
+          <span className="span-yellowText"> Bezpečnos</span> - escapování,
+          trimování, hashování hesel a prevence XSS.
+        </p>
+        <p>
+          <span className="span-yellowText">OOP</span> a využití vestavěných
+          funkcí.
         </p>
       </article>
     ),
   },
   {
     id: 6,
-    buttonValue: "Node.js",
+    buttonValue: "TypeScript",
     jsxValue: (
       <article className="oneLanguageDetail">
-        <h1>Node.js - Express</h1>
+        <h1>TypeScript</h1>
         <h2>Při práci s ním ovládám </h2>
         <p>
           <span className="span-yellowText">Datové typy</span> - Literal type,
